9a27a70f82a332595d6da098119c84fd21070f24
Daniel <danielosiro@Hotmail.com>
2018-02-01 11:29:50 -0200

    html form

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt">
+
+  <form>
+    Login: <br>
+    <input type="text" name="Login"> <br>
+    Password: <br>
+    <input type="password" name="Password"> <br>
+    <input type="submit" value="submit">
+  </form>
+
+</html>
