89ccb197c9f169aa78ed57e4235a7f314aa7d8b5
Daniel <danielosiro@Hotmail.com>
2018-02-01 20:23:19 -0200

    login and password js validadtion (not working)

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,15 @@ $(document)
 .on("submit", "form.register", function(event){
   event.preventDefault();
 
-  alert('Form was submitted.');
+  var _form = $(this);
+
+  var data = {
+    login: $("input[type='text']", _form).val(),
+    password: $("input[type='password']", _form).val()
+  }
+
+  console.log(data);
+  alert(data);
 
   return false;
 })
