23fa4f35fa383d08c2868cf70dc3f84b2dd72492
Daniel <danielosiro@Hotmail.com>
2018-02-02 17:20:51 -0200

    Fixed validation

--- ajax/login.php
+++ ajax/login.php
@@ -39,23 +39,7 @@
     } else{
       //User does not exist, offer to register
       $return['error'] = "You do not have an account. <a href='/register.php'>Create one now?</a>";
-      $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-
-      $addUser = $con->prepare("INSERT INTO users(email, password) VALUES (LOWER(:email), :password)");
-      $addUser->bindParam(':email', $email, PDO::PARAM_STR);
-      $addUser->bindParam(':password', $password, PDO::PARAM_STR);
-      $addUser->execute();
-
-      $user_id = $con->lastInsertId();
-
-      $_SESSION['user_id'] = (int) $user_id;
-
-      $return['redirect'] = '/dashboard.php?message=welcome';
-      $return['is_logged_in'] = true;
     }
-    //Make sure the user CAN be added and IS added
-
-    //Return the proper information back to javascript to redirect us
 
     echo json_encode ($return, JSON_PRETTY_PRINT); exit;
   } else{
