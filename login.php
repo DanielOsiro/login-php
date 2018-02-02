7ddf9733a7278737509f945cb158c88223ab1f16
Daniel <danielosiro@Hotmail.com>
2018-02-02 16:50:05 -0200

    Added login ajax

--- login.php
+++ login.php
@@ -8,13 +8,16 @@
 <!DOCTYPE html>
 <html lang="pt">
 
-  <form>
+  <form class="js-login">
     <h1>Login</h1>
 
     E-mail: <br>
     <input type="text" name="Login"> <br>
     Password: <br>
     <input type="password" name="password"> <br>
+
+    <p class="js-error" style="display: none"></p>
+
     <input type="submit" value="submit">
   </form>
 
