1d9a395af3cd7b6082c5c4999bd0f597816dc646
Daniel <danielosiro@Hotmail.com>
2018-02-02 19:06:04 -0200

    Tiny css

--- index.php
+++ index.php
@@ -8,18 +8,27 @@
 <!DOCTYPE html>
 <html lang="pt">
   <head>
+    <style>
+      .center{
+        text-align: center;
+      }
+    </style>
   </head>
 
   <body>
-    <?php
-      echo "<h1>PHP login system</h1>";
-      echo "<p>This is a php-login system</p>"
-    ?>
+    <div style="margin:auto; width:50%; border: 2px solid black; padding: 70px 0;">
+      <?php
+        echo "<h1 class='center'>PHP login system</h1>";
+        echo "<br>";
+        echo "<p class='center'>This is a php-login system</p>";
+        echo "<br>";
+      ?>
 
-    <p>
-      <a href="login.php">Login</a>
-      <a href="register.php">Register</a>
-    </p>
+      <p>
+        <a href="login.php">Login</a>
+        <a href="register.php">Register</a>
+      </p>
+    </div>
 
     <?php require_once "inc/footer.php"; ?>
   </body>
