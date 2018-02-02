76fc43ea863e93f187f81a10ecf395774d40c764
Daniel <danielosiro@Hotmail.com>
2018-02-02 19:20:48 -0200

    Dashboard user comfirmation

--- dashboard.php
+++ dashboard.php
@@ -10,26 +10,16 @@
 <!DOCTYPE html>
 <html lang="pt">
   <head>
-    <style>
-      .center{
-        text-align: center;
-      }
-    </style>
+    <title>
+      <?php
+        echo 'User ' . $_SESSION['user_id'];
+      ?>
+    </title>
   </head>
 
   <body>
     <div style="margin:auto; width:50%; border: 2px solid black; padding: 70px 0;">
-      <?php
-        echo "<h1 class='center'>PHP login system</h1>";
-        echo "<br>";
-        echo "<p class='center'>This is a php-login system</p>";
-        echo "<br>";
-      ?>
-
-      <p>
-        <a href="login.php">Login</a>
-        <a href="register.php">Register</a>
-      </p>
+      <p style="text-align: center;">Your are now loged in.</p>
     </div>
 
     <?php require_once "inc/footer.php"; ?>
