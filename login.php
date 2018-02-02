dd2116c7f19996a9ab014d43f24bc054074101c1
Daniel <danielosiro@Hotmail.com>
2018-02-02 18:21:57 -0200

    Working login validation

--- login.php
+++ login.php
@@ -9,30 +9,25 @@
 <html lang="pt">
 
   <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="robots" content="follow">
-
-    <title>Page Title</title>
-
-    <base href="/" />
   </head>
 
   <body>
+
     <form class="js-login">
       <h1>Login</h1>
 
       E-mail: <br>
-      <input type="text" name="Login"> <br>
+      <input type="email" required="required"> <br>
       Password: <br>
-      <input type="password" name="password"> <br>
+      <input type="password" required="required"> <br>
 
       <p class="js-error" style="display: none"></p>
 
-      <input type="submit" value="submit">
+      <input type="submit" value="Login">
     </form>
 
     <?php require_once "inc/footer.php"; ?>
+
   </body>
 
 </html>
