3c807f6430157be6e3987cca5b4c966544d1d396
Daniel <danielosiro@Hotmail.com>
2018-02-02 09:17:03 -0200

    Inserts into table, Checks session

--- ajax/register.php
+++ ajax/register.php
@@ -10,6 +10,8 @@
 
     $return = [];
 
+    $email = $_POST['email'];
+
     //Make sure the user does not exist
     $findUser = $con->prepare("SELECT user_id FROM users WHERE email = LOWER(:email) LIMIT 1");
     $findUser->bindParam(':email', $email, PDO::PARAM_STR);
@@ -19,23 +21,32 @@
       //User exists
       //Check if the user can log in
       $return['error'] = "You already have an account";
+      $return['is_logged_in'] = false;
     } else{
       //User does not exist, add them now
-      $addUser = $con->prepare("INSERT INTO users(email, password) VALUES (:email, :password)");
+
+      $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+      $addUser = $con->prepare("INSERT INTO users(email, password) VALUES (LOWER(:email), :password)");
       $addUser->bindParam(':email', $email, PDO::PARAM_STR);
       $addUser->bindParam(':password', $password, PDO::PARAM_STR);
       $addUser->execute();
+
+      $user_id = $con->lastInsertId();
+
+      $_SESSION['user_id'] = (int) $user_id;
+
+      $return['redirect'] = '/dashboard.php?message=welcome';
+      $return['is_logged_in'] = true;
     }
     //Make sure the user CAN be added and IS added
 
     //Return the proper information back to javascript to redirect us
 
-    $return['redirect'] = '/index.php?this-was-a-redirect';
-
     echo json_encode ($return, JSON_PRETTY_PRINT); exit;
   } else{
     //Kill the script. Redirect user.
-    exit('test');
+    exit('Invalid URL');
   }
 
 ?>
